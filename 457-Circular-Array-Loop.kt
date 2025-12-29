77088f88ac8e97809f1a6335e7fb3274dcd23e65
cprayer <cprayer13@gmail.com>
2025-12-30 03:08:02 +0900

    457. Circular Array Loop(optimal)

--- 457-Circular-Array-Loop.kt
+++ 457-Circular-Array-Loop.kt
@@ -14,19 +14,18 @@ class Solution {
             if (nums[i] == 0) {
                 continue
             }
-            val absNum = abs(nums[i])
-            if (absNum.and(visitedBit) == visitedBit) {
-                continue
-            }
 
             var current = i
             val isPositive = nums[current] > 0
 
-            var count = 1
-            while(count <= nums.size) {
-                val absCurrentNum = abs(nums[current])
-                nums[current] = if(isPositive) absCurrentNum.or(visitedBit) else -(absCurrentNum.or(visitedBit))
-                val next = (current + (nums[current].xor(visitedBit)) + nums.size * 10_000) % nums.size
+            var count = 0
+            while(count < nums.size) {
+                if (abs(nums[current]).and(visitedBit) == visitedBit) {
+                    count = -1
+                    break
+                }
+
+                val next = (current + nums[current] + nums.size * 10_000) % nums.size
 
                 current = if (isPositive) {
                     if (nums[next] > 0) {
@@ -46,8 +45,32 @@ class Solution {
                 count += 1
             }
 
-            if (count == nums.size + 1) {
+            if (count == nums.size) {
                 return true
+            }
+
+            current = i
+            while(count < nums.size) {
+                val currentNum = nums[current]
+                if (abs(currentNum).and(visitedBit) == visitedBit) {
+                    break
+                }
+                val next = (current + currentNum + nums.size * 10_000) % nums.size
+                nums[current] = if (currentNum > 0) currentNum.or(visitedBit) else -(abs(currentNum).or(visitedBit))
+
+                current = if (isPositive) {
+                    if (nums[next] > 0) {
+                        next
+                    } else {
+                        break
+                    }
+                } else {
+                    if (nums[next] < 0) {
+                        next
+                    } else {
+                        break
+                    }
+                }
             }
         }
         return false
