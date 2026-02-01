82fad81ee76c008fe5d863b9794ba173afe1e949
cprayer <cprayer13@gmail.com>
2026-02-01 13:07:25 +0900

    221-Maximal-Square(min(N, M) * N * M) 올바른 코드로 반영

--- 221-Maximal-Square(min(N, M) * N * M).kt
+++ 221-Maximal-Square(min(N, M) * N * M).kt
@@ -1,23 +1,41 @@
-import java.lang.Integer.max
 import java.lang.Integer.min
 
 class Solution {
     fun maximalSquare(matrix: Array<CharArray>): Int {
         val height = matrix.size
         val width = matrix[0].size
-        val dp = Array(height + 1) { IntArray(width + 1) }
+        val maximal = min(height, width)
+        val dp = Array(maximal) { Array(height + 1) { IntArray(width + 1) } }
 
         var ans = 0
-        for (i in 1..height) {
-            for (j in 1..width) {
-                if (matrix[i - 1][j - 1] == '0') {
-                    continue
+        for (i in 0 until height) {
+            for (j in 0 until width) {
+                dp[0][i][j] = when(matrix[i][j]) {
+                    '1' -> 1
+                    else -> 0
                 }
-                dp[i][j] = min(min(dp[i - 1][j - 1], dp[i - 1][j]), dp[i][j - 1]) + 1
-                ans = max(ans, dp[i][j] * dp[i][j])
+                if (dp[0][i][j] == 1) {
+                    ans = 1
+                }
             }
         }
 
+        for (m in 1 until maximal) {
+            for (i in 0 until height) {
+                for (j in 0 until width) {
+                    val sum = dp[m - 1][i][j] + dp[m - 1][i][j + 1] + dp[m - 1][i + 1][j] + dp[m - 1][i + 1][j + 1]
+                    dp[m][i][j] = if (sum == 4) {
+                        1
+                    } else {
+                        0
+                    }
+                    if (dp[m][i][j] == 1) {
+                        ans = (m + 1) * (m + 1)
+                    }
+                }
+            }
+        }
+        
         return ans
     }
 }
