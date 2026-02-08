a37be072d4c495feecbf3a59a0baae51ad409e22
cprayer <cprayer13@gmail.com>
2026-02-08 12:52:50 +0900

    80. Remove Duplicates from Sorted Array II(개선 2)

--- 80-Remove-Duplicates-from-Sorted-Array-II.kt
+++ 80-Remove-Duplicates-from-Sorted-Array-II.kt
@@ -1,28 +1,11 @@
 class Solution {
     fun removeDuplicates(nums: IntArray): Int {
-        var currentIndex = 0
-        while (currentIndex < nums.size) {
-            val num = nums[currentIndex]
-            var count = 0
-            while (currentIndex < nums.size && num == nums[currentIndex]) {
-                count += 1
-                if (count >= 3) {
-                    nums[currentIndex] = Int.MIN_VALUE
-                }
-                currentIndex += 1
+        var write = 0
+        for (x in nums) {
+            if (write < 2 || nums[write - 2] != x) {
+                nums[write++] = x
             }
         }
-
-        var writeIndex = 0
-        for (num in nums) {
-            if (num == Int.MIN_VALUE) {
-                continue
-            }
-            nums[writeIndex] = num
-            writeIndex += 1
-        }
-
-        return writeIndex
+        return write
     }
 }
-
