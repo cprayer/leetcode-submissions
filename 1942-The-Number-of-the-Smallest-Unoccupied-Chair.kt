10dc7ea2a6805e15de8789000337edf4bcea3e1d
cprayer <cprayer13@gmail.com>
2026-09-25 03:06:35 +0900

    1296. Divide Array in Sets of K Consecutive Numbers

--- 1942-The-Number-of-the-Smallest-Unoccupied-Chair.kt
+++ 1942-The-Number-of-the-Smallest-Unoccupied-Chair.kt
@@ -1,30 +1,57 @@
-import java.util.PriorityQueue
-
 class Solution {
-    fun smallestChair(times: Array<IntArray>, targetFriend: Int): Int {
-        val friends = times.indices.sortedBy { times[it][0] }
-        val occupied = PriorityQueue<Pair<Int, Int>>(compareBy { it.first })
-        val available = PriorityQueue<Int>()
-        var nextChair = 0
-
-        for (friend in friends) {
-            val (arrival, leaving) = times[friend]
-
-            while (occupied.isNotEmpty() && occupied.peek().first <= arrival) {
-                available.offer(occupied.poll().second)
-            }
-
-            val chair = if (available.isNotEmpty()) {
-                available.poll()
-            } else {
-                nextChair++
-            }
-
-            if (friend == targetFriend) return chair
-
-            occupied.offer(leaving to chair)
+    fun isPossibleDivide(nums: IntArray, k: Int): Boolean {
+        if (nums.size % k != 0) {
+            return false
         }
 
-        return -1
+        val counts = mutableMapOf<Int, Int>()
+        for (num in nums) {
+            counts[num] = counts.getOrDefault(num, 0) + 1
+        }
+
+        val startPoints = mutableListOf<Int>()
+        for (key in counts.keys) {
+            if (counts.getOrDefault(key - 1, 0) == 0) {
+                startPoints.add(key)
+            }
+        }
+
+        for (start in startPoints) {
+            val candidates = ArrayDeque<Int>()
+            candidates.add(start)
+            while (candidates.isNotEmpty()) {
+                val current = candidates.removeFirst()
+                val currentCount = counts.getOrDefault(current, 0)
+                if (currentCount == 0) {
+                    continue
+                }
+
+                var isAdded = false
+                for (i in 0 until k) {
+                    val count = counts.getOrDefault(current + i, 0)
+                    if (count < currentCount) {
+                        return false
+                    }
+                    counts[current + i] = count - currentCount
+                    if (count - currentCount > 0) {
+                        candidates.add(current + i)
+                        isAdded = true
+                    }
+                }
+                if (!isAdded) {
+                    if (counts.getOrDefault(current + k, 0) > 0) {
+                        candidates.add(current + k)
+                    }
+                }
+            }
+        }
+
+        for (num in nums) {
+            if (counts[num] != 0) {
+                return false
+            }
+        }
+
+        return true
     }
 }
